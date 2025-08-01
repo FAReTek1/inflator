60924e600038239afd130d6198b45b16a425aedc
faretek <ldashunderscoreo@gmail.com>
2025-08-01 20:35:53 +0900

    feat: reset

--- inflator/gstoml.py
+++ inflator/gstoml.py
@@ -1,6 +1,8 @@
 # If we cannot locate a inflator.toml file, try to resolve stuff from goboscript.toml
 # Note that these packages will need to be imported into backpack/ instead of inflate/
 
+
+
 toml_return = tuple[dict, dict[str, dict[str, str]]]
 
 
